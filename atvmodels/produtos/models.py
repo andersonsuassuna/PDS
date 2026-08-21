33134459d8b9b317ea06e6699689049a563bc943
Anderson Suassuna <andersonssgf7@gmail.com>
2026-08-21 14:23:09 -0300

    atvmodels

--- atvmodels/produtos/models.py
+++ atvmodels/produtos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Produto(models.Model):
+   nome = models.CharField(max_length=100)
+   categoria = models.CharField(max_length=20)
+   quantidade = models.IntegerField()
+
+   def __str__(self):
+       return self.nome
